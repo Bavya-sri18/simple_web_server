c41f03a7a33cbf7f718b40252469dfd08d86a0ed
bala yogesh <bavyasribalamurugan@gmail.com>
2024-12-04 17:48:27 +0530

    success

--- web.py
+++ web.py
@@ -1,29 +1,3 @@
-# EX01 Developing a Simple Webserver
-
-# Date: 04.12.2024
-# AIM:
-To develop a simple webserver to serve html pages and display the configuration details of laptop.
-
-# DESIGN STEPS:
-## Step 1:
-HTML content creation.
-
-## Step 2:
-Design of webserver workflow.
-
-## Step 3:
-Implementation using Python code.
-
-## Step 4:
-Serving the HTML pages.
-
-## Step 5:
-Testing the webserver.
-
-# PROGRAM:
-```
-
-
 from django.shortcuts import render,HttpResponse
 from http.server import HTTPServer,BaseHTTPRequestHandler
 def trial(request):
@@ -129,10 +103,3 @@ print("This is my webserver")
 server_address=('',8000)
 httpd= HTTPServer(server_address,MyServer)
 httpd.serve_forever()
-```
-# OUTPUT:
-![alt text](<device specification.png>)
-
-![alt text](terminal.png)
-# RESULT:
-The program for implementing simple webserver is executed successfully.
